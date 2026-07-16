9845e2976ace344a2feb482a235a977fe3c7202a
techstack-io <dcollins31@gmail.com>
2026-07-16 13:46:57 -0500

    Replace dandelion animation with magic particles

---
+++ memoapp/components/MagicParticles.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Lottie from "lottie-react";
+import animationData from "@/public/animations/magic_particles.json";
+
+type MagicParticlesProps = {
+  className?: string;
+};
+
+export default function MagicParticles({
+  className = "",
+}: MagicParticlesProps) {
+  return (
+    <div className={className} aria-hidden="true">
+      <Lottie
+        animationData={animationData}
+        loop
+        autoplay
+        className="h-full w-full"
+      />
+    </div>
+  );
+}
