772d30508fcc28d73dfbe1a5e82f3b544fe02090
techstack-io <dcollins31@gmail.com>
2026-07-22 15:50:10 -0500

    Import additional icons in MemoMindDashboard

--- memoapp/components/dashboard/MemoMindDashboard.tsx
+++ memoapp/components/dashboard/MemoMindDashboard.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import AppSidebar from "@/components/layout/AppSidebar";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, BookOpen, Compass, Mountain } from "lucide-react";
 
 type MemoMindDashboardProps = {
   userName?: string;
