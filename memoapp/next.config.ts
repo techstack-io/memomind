7e31e4b0a39faa7c90036338fd839b46fb89db3d
techstack-io <dcollins31@gmail.com>
2026-07-28 20:57:09 -0500

    Added /waitlist reroute

--- memoapp/next.config.ts
+++ memoapp/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/waitlist",
+        destination: "https://www.joinmettavia.online/",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
