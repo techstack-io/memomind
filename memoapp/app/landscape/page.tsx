70436ee8be761b752a233ecf8b07d825266f8c76
techstack-io <dcollins31@gmail.com>
2026-07-31 19:49:02 -0500

    Add landscape image prototype

--- memoapp/app/landscape/page.tsx
+++ memoapp/app/landscape/page.tsx
@@ -2,15 +2,17 @@ import Image from "next/image";
 
 export default function LandscapePage() {
   return (
-    <main className="relative min-h-screen overflow-hidden bg-[#F3EEE5]">
-      <Image
-        src="/landscape/hero.jpeg"
-        alt=""
-        fill
-        priority
-        sizes="100vw"
-        className="object-cover object-center"
-      />
+    <main className="relative flex min-h-screen items-center justify-center overflow-hidden bg-[#F3EEE5]">
+      <div className="relative h-[75vh] w-[75vw]">
+        <Image
+          src="/landscape/hero.jpeg"
+          alt=""
+          fill
+          priority
+          sizes="75vw"
+          className="object-contain"
+        />
+      </div>
     </main>
   );
 }
